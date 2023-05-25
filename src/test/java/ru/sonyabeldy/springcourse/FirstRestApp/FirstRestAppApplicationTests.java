e162c9936c3bdd51916a53f950ea93fceca1d185
Sonya <sofya.beldy@mail.ru>
2023-05-25 22:35:38 +1000

    first commit

---
+++ src/test/java/ru/sonyabeldy/springcourse/FirstRestApp/FirstRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sonyabeldy.springcourse.FirstRestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
