f8a8c85488582843bd3ed17881cb981710701f00
Sonya <sofya.beldy@mail.ru>
2023-05-26 16:49:15 +1000

    get request

--- src/main/java/ru/sonyabeldy/springcourse/FirstRestApp/controllers/PeopleController.java
+++ src/main/java/ru/sonyabeldy/springcourse/FirstRestApp/controllers/PeopleController.java
@@ -1,5 +1,34 @@
 package ru.sonyabeldy.springcourse.FirstRestApp.controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.sonyabeldy.springcourse.FirstRestApp.models.Person;
+import ru.sonyabeldy.springcourse.FirstRestApp.services.PeopleService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/people")
 public class PeopleController {
 
+    public final PeopleService peopleService;
+
+    @Autowired
+    public PeopleController(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
+    @GetMapping
+    public List<Person> getPeople() {
+        return peopleService.findAll(); //Jackson конвертирует эти объекты в JSON
+    }
+
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable("id") int id) {
+        return peopleService.findOne(id); //jackson автоматически конвертирует в JSON
+    }
 }
